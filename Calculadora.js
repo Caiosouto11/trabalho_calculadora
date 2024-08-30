a781e1cfc74b3b5514f19ee8c5437b4386022b9a
Caiosouto11 <caiosouto006@gmail.com>
2024-08-30 20:39:23 -0300

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,20 @@
+let numero1 = 2
+let numero2 = 6
+
+let ad = numero1 + numero2
+let sub = numero1 - numero2
+let mult = numero1 * numero2
+let div = numero1 / numero2
+
+
+if (numero2 == 0){
+    console.log('Numero 2 invalido')
+    return
+}
+console.log (`soma = ${ad}`)
+console.log (`subtração = ${sub}`)
+console.log('Multiplicação ='+ ' '+ mult)
+console.log('divisão =' +' ' +  div)
+
+
+console.log('caiosouto')
